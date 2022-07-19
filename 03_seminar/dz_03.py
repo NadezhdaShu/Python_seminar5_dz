ee781e2d2e9541fdc115248be52620858e17b7ff
ShuryginaNad <shnadya@yandex.ru>
2022-07-19 21:28:48 +0300

    zad 3 new

--- 03_seminar/dz_03.py
+++ 03_seminar/dz_03.py
@@ -3,3 +3,17 @@
 
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+list = [1.1, 1.2, 3.1, 5.61, 10.01]
+min_val = int(str(list[0]).split('.')[1])
+max_val = int(str(list[0]).split('.')[1])
+
+for i in range(len(list)):
+    vrem_val = int(str(list[i]).split('.')[1])
+    if(min_val > vrem_val):
+        min_val = vrem_val
+    if(max_val < vrem_val):
+        max_val = vrem_val
+
+print(f"Наибольший: {max_val} \r\nНаименьший: {min_val}")
+print(f"Разница между наибольшим и наименьшим: {max_val - min_val}")
